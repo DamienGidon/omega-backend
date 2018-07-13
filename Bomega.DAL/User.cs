4baaf538e3ef4a04baa2640c907e1b5034e3abbf
DamienGidon <dgidon@intechinfo.fr>
2018-07-13 15:54:57 +0200

    add back

---
+++ Bomega.DAL/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Bomega.DAL
+{
+    public class User : TableEntity
+    {
+        public string Guid { get; set; }
+        public string IdSpotify { get; set; }
+        public string IdDeezer { get; set; }
+        public string IdFacebook { get; set; }
+        public string Name { get; set; }
+    }
+}
